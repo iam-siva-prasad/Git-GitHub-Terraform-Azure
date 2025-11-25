a2601e1840e5380ff68c897a159560252aa4df7c
SIVA PRASAD <golisiva50@gmail.com>
2025-11-25 11:58:39 +0530

    Updated the Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,16 +1,19 @@
+
 terraform {
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.47.0" # Use latest stable
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    msgraph = {
+      source  = "microsoftgraph/msgraph"
+      version = "~> 0.2.0" # Check latest version
     }
   }
 }
 
-provider "azuread" {
-  tenant_id     = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
+provider "azurerm" {
+  features {}
 }
 
 provider "msgraph" {
@@ -18,4 +21,30 @@ provider "msgraph" {
   client_id     = var.client_id
   client_secret = var.client_secret
 }
+
+
+
+
+
+###############################################################################################
+# terraform {
+#   required_providers {
+#     azuread = {
+#       source  = "hashicorp/azuread"
+#       version = "~> 2.47.0" # Use latest stable
+#     }
+#   }
+# }
+
+# provider "azuread" {
+#   tenant_id     = var.tenant_id
+#   client_id     = var.client_id
+#   client_secret = var.client_secret
+# }
+
+# # provider "msgraph" {
+# #   tenant_id     = var.tenant_id
+# #   client_id     = var.client_id
+# #   client_secret = var.client_secret
+# # }
 
