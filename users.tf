fdf8f42d3030b19963f48d53a95df7b5f72499a9
SIVA PRASAD <golisiva50@gmail.com>
2025-11-25 12:02:48 +0530

    just updated the user.tf file!!!

--- users.tf
+++ users.tf
@@ -1,17 +1,6 @@
 resource "azuread_user" "example" {
-  user_principal_name = "terra2025@76hpc3.onmicrosoft.com"
-  display_name        = "terraform-2025"
+  user_principal_name = "Git-GitHub-Terraform-Azure@76hpc3.onmicrosoft.com"
+  display_name        = "Git-GitHub-Terraform-Azure"
   password            = "ComplexPassword123!"
   force_password_change = true
 }
-
-resource "azuread_user_license" "example" {
-  user_object_id = azuread_user.example.object_id
-  license_type   = "Microsoft Power Automate Free" # Example: Office 365 E3
-}
-
-# resource "azuread_group_member" "example" {
-#   group_object_id = azuread_group.example.id
-#   member_object_id = azuread_user.example.id
-# }
-
