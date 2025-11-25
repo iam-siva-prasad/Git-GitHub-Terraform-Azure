98cf84f5f58be582693003d2b229332ff07432ba
SIVA PRASAD <55012125+siva9110@users.noreply.github.com>
2025-11-25 15:45:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
 
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
