6a5244fad11ff178b4d0414abdfaa9230c58def8
SIVA PRASAD <55012125+siva9110@users.noreply.github.com>
2025-11-25 16:02:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,28 +1,28 @@
-# output "user_principal_name" {
-#     value = azuread_user.Terraform.user_principal_name  
-# }
+ output "user_principal_name" {
+     value = azuread_user.Terraform.user_principal_name  
+ }
 
-# output "user_object_id" {
+ output "user_object_id" {
 
-#   value = azuread_user.Terraform.object_id
+   value = azuread_user.Terraform.object_id
   
-# }
+ }
 
-output "user_principal_name" {
+output "user_principal_name_terraform" {
     value = azuread_user.Terraform-1.user_principal_name   
 }
 
-output "user_object_id" {
+output "user_object_id_terraform" {
 
   value = azuread_user.Terraform-1.object_id
   
 }
 
-output "user_principal_name" {
+output "user_principal_name_git" {
     value = azuread_user.git.user_principal_name   
 }
 
-output "user_object_id" {
+output "user_object_id_git" {
 
   value = azuread_user.git.object_id
   
