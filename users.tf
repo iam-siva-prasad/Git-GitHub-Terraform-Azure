b85002592a10b6b05edb589a8926a8cbd6fa873e
SIVA PRASAD <55012125+siva9110@users.noreply.github.com>
2025-11-25 16:51:38 +0530

    Update users.tf

--- users.tf
+++ users.tf
@@ -1,27 +1,27 @@
 resource "azuread_user" "user1" {
-  user_principal_name = "Git-GitHub-Terraform-Azure@76hpc3.onmicrosoft.com"
-  display_name        = "Git-GitHub-Terraform-Azure"
+  user_principal_name = "9110Azure@76hpc3.onmicrosoft.com"
+  display_name        = "9110azure"
   password            = "ComplexPassword123!"
   force_password_change = true
 }
 
 resource "azuread_user" "user2" {
-  user_principal_name = "1Git-GitHub-Terraform-Azure@76hpc3.onmicrosoft.com"
-  display_name        = "1-GitHub-Terraform-Azure"
+  user_principal_name = "user2Azure@76hpc3.onmicrosoft.com"
+  display_name        = "user2Azure"
   password            = "ComplexPassword123!"
   force_password_change = true
 }
 
 resource "azuread_user" "user3" {
-  user_principal_name = "Git@76hpc3.onmicrosoft.com"
-  display_name        = "Git"
+  user_principal_name = "gituser3@76hpc3.onmicrosoft.com"
+  display_name        = "Gituser3"
   password            = "ComplexPassword123!"
   force_password_change = true
 }
 
 resource "azuread_user" "user4" {
-  user_principal_name = "user4@76hpc3.onmicrosoft.com"
-  display_name        = "user4"
+  user_principal_name = "gituser4@76hpc3.onmicrosoft.com"
+  display_name        = "user4git"
   password            = "ComplexPassword123!"
   force_password_change = true
 }
