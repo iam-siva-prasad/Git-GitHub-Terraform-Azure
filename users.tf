c00922e825498def37ae1e9058f07f09da4868d1
SIVA PRASAD <55012125+iam-siva-prasad@users.noreply.github.com>
2025-12-07 12:21:42 +0530

    Update users.tf

--- users.tf
+++ users.tf
@@ -4,9 +4,4 @@ resource "azuread_user" "user5" {
   password            = "ComplexPassword123!"
   force_password_change = true
 }
-resource "azuread_user" "muni" {
-  user_principal_name = "muneendra@76hpc3.onmicrosoft.com"
-  display_name        = "muneendra"
-  password            = "ComplexPassword123!"
-  force_password_change = true
-}
+
